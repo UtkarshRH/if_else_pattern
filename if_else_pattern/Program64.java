e21a545d1786b1ac92d64c67132b90033950ca18
Utkarsh Harshe <103624983+UtkarshRH@users.noreply.github.com>
2023-09-24 16:09:36 +0530

    Diamond Pattern source code

---
+++ if_else_pattern/Program64.java
@@ -0,0 +1,19 @@
+package if_else_pattern;
+
+public class Program64 {
+    public static void main(String args[]) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print("*");
+            }
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
