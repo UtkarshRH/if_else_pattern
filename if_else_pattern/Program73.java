e0c1c18a1d45182f1e447307d87842b0196e7cbf
Utkarsh Harshe <103624983+UtkarshRH@users.noreply.github.com>
2023-09-25 00:44:27 +0530

    Pattern Program Source code

---
+++ if_else_pattern/Program73.java
@@ -0,0 +1,19 @@
+package if_else_pattern;
+
+public class Program73 {
+    public static void main(String args[]) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j > 1; j--) {
+                System.out.print(" ");
+            }
+            for (int j = i; j <= n; j++) {
+                System.out.print(j);
+            }
+            for (int j = n - 1; j >= i; j--) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
